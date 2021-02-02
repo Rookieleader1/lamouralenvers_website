db09629deac5bed32c1a52525b19d56c59220364
A.Geffrelot <alexandre.geffrelot@xxii.fr>
2021-02-02 11:12:25 +0100

    cleaned up routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./style/output.css";
 import AudioPlayer from "./Components/AudioPlayer";
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
 
 const App = () => {
   // return (
@@ -13,22 +19,11 @@ const App = () => {
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/QRcode1">QRcode1</Link>
-            </li>
-          </ul>
-        </nav>
-
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
           <Route path="/QRcode1">
-            <div>QR code 1</div>
+            <Redirect to="/" />
           </Route>
           <Route path="/">
             <AudioPlayer />
